fa4b64ac86a62b89619386a36c6e253ac09094f9
GUO MING <2994435150@qq.com>
2022-04-15 14:42:31 +0800

    swain push authorization server and gatewayserver with sentineland oauth2

---
+++ 03_代码仓库/chinese_run/crwms_iaas/authorization-server/src/main/java/com/advgm/config/AuthorizationServerConfig.java
@@ -0,0 +1,48 @@
+package com.advgm.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@EnableAuthorizationServer
+@Configuration
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    /**
+     * 加载第三方客户端
+     */
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("")//第三方客户端的名称
+                        .secret(passwordEncoder.encode("cr-secret"))//第三方客户端的密钥
+                                .scopes("all")//第三方客户端的授权范围
+                .accessTokenValiditySeconds(3600)//token的有效期
+                        .refreshTokenValiditySeconds(7*3600);//重新刷新token的时间
+
+        super.configure(clients);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
+        super.configure(endpoints);
+    }
+}
---
+++ 03_代码仓库/chinese_run/crwms_iaas/gateway-server/src/main/java/com/advgm/controller/GatewayFlowRulesController.java
@@ -0,0 +1,24 @@
+package com.advgm.controller;
+
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiDefinition;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.GatewayApiDefinitionManager;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+public class GatewayFlowRulesController {
+    @GetMapping("/gw/flow/rules")
+    public Set<GatewayFlowRule> getCurrentGatewayFlowRules(){
+        return GatewayRuleManager.getRules();
+    }
+
+    @GetMapping("/gw/api/groups")
+    public Set<ApiDefinition> getApiGroups(){
+        return GatewayApiDefinitionManager.getApiDefinitions();
+    }
+
+}
