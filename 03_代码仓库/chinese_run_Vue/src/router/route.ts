8786f5c56a0c698f01046df6dbcdad873d68d4c0
huanghaoming <986667872@qq.com>
2022-05-10 12:20:48 +0800

    driver
    司机管理

--- 03_代码仓库/chinese_run_Vue/src/router/route.ts
+++ 03_代码仓库/chinese_run_Vue/src/router/route.ts
@@ -87,6 +87,14 @@ export const staticRoutes: Array<RouteRecordRaw> = [
 				}
 			},
 			{
+				path:'/driver',
+				name: 'Driver',
+				component: ()=>import("/@/views/system/driver/index.vue"),
+				meta:{
+					title: '司机管理'
+				}
+			},
+			{
 				path: '/online/cgform',
 				name: 'CgForm',
 				component: ()=>import("/@/views/online/cgform/index.vue"),
