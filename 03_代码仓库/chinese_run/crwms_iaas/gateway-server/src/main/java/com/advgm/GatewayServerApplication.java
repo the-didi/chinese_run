6b6a638851e54969f744c8bed4d6ba10f1727af2
huanghaoming <986667872@qq.com>
2022-06-09 22:17:19 +0800

    文档更新

--- 03_代码仓库/chinese_run/crwms_iaas/gateway-server/src/main/java/com/advgm/GatewayServerApplication.java
+++ 03_代码仓库/chinese_run/crwms_iaas/gateway-server/src/main/java/com/advgm/GatewayServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class    GatewayServerApplication {
+public class  GatewayServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServerApplication.class,args);
     }
